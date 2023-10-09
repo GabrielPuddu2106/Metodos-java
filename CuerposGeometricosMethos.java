40a006e0c06fcdab4f65c2d955e5301d3bbfacdc
GabrielPuddu2106 <142625919+GabrielPuddu2106@users.noreply.github.com>
2023-10-09 17:45:05 -0600

    commit21

---
+++ CuerposGeometricosMethos.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class CuerposGeometricosMethos{
+    static Scanner lector  = new Scanner(System.in);
+
+    static double areaLateral, areaTotal, volumen, tang, aBase, area, volumenZona, radio, volumenCasquete;
+    static double  pBase, apotema, areaBase, altura, diametro,longitud, baseMenor;
+    static  double ladoMenor, ladoMayor, areaBMAYOR, areaBMENOR, pBMAYOR, pBMENOR;
+    static  double radioMayor, radioMenor, generatriz;
+    static int lados, base, numeroGrados, caras, arista;
+    static final double pii = 3.1416;
+
+    public static void main(String []args){
+        System.out.println("Menu de opciones");
+
+        System.out.println("1) Prisma");
+
+        System.out.println("2) Cilindro");
+
+        System.out.println("3) Piramide");
+
+        System.out.println("4) Cono");
+
+        System.out.println("5) Esfera");
+
+        System.out.println("6) ZonaEsferica");
+
+        System.out.println("7) CasqueteEsferico");
+
+        System.out.println("8) TroncoDeCono");
+
+        System.out.println("9) TroncoDePiramide");
+
+        System.out.println("10) Hueso_CuñaEsferica");
+
+        System.out.println("11) Ortoedro");
+
+        System.out.println("12) Tetaedro");
+
+        System.out.println("13) Hexaedro");
+
+        System.out.println("14) Octaedro");
+
+        System.out.println("15) Dodecaedro");
+
+        System.out.println("16) Icosaedro");
+
+        System.out.println("17) Salir");
+
+        byte opc = lector.nextByte();
+
+        switch (opc) {
+            case 1: 
+                 solucionPrisma
+
+    }
+
+
+}
